ce2678636408957a97d7b6ddaecb78656d659fab
Wagner Tavares <wagner_madara@hotmail.com>
2018-09-22 15:20:00 -0300

    new file

--- Code/GoogleVoiceSpeech.cs
+++ Code/GoogleVoiceSpeech.cs
@@ -25,182 +25,166 @@
 //
 using UnityEngine;
 using System;
-using System.IO;
 using System.Collections;
-using System.Collections.Generic;
-using System.Net;
+using UnityEngine.UI;
+//using coti_VR;
+using UnityEngine.Networking;
+using System.IO;
 
-[RequireComponent (typeof (AudioSource))]
 
-public class GoogleVoiceSpeech : MonoBehaviour {
+[RequireComponent(typeof(AudioSource))]
 
-		public GUIText TextBox;
+public class GoogleVoiceSpeech : MonoBehaviour
+{
 
-		struct ClipData
-		{
-				public int samples;
-		}
+    const int HEADER_SIZE = 44;
 
-		const int HEADER_SIZE = 44;
+    private int minFreq;
+    private int maxFreq;
 
-		private int minFreq;
-		private int maxFreq;
+    string Response;
+    public static string resultString = null;
 
-		private bool micConnected = false;
+    private bool micConnected = false;
 
-		//A handle to the attached AudioSource
-		private AudioSource goAudioSource;
+    //A handle to the attached AudioSource
+    public static AudioSource goAudioSource;
+    public Text res; //references to text Obj
+    public static Text res_;
 
-		public string apiKey;
+    public static string filePath;
 
-	// Use this for initialization
-	void Start () {
-				//Check if there is at least one microphone connected
-				if(Microphone.devices.Length <= 0)
-				{
-						//Throw a warning message at the console if there isn't
-						Debug.LogWarning("Microphone not connected!");
-				}
-				else //At least one microphone is present
-				{
-						//Set 'micConnected' to true
-						micConnected = true;
+    // Use this for initialization
+    void Start()
+    {
 
-						//Get the default microphone recording capabilities
-						Microphone.GetDeviceCaps(null, out minFreq, out maxFreq);
+        res_ = res;
+        //Check if there is at least one microphone connected
+        if (Microphone.devices.Length <= 0)
+        {
+            //Throw a warning message at the console if there isn't
+            Debug.LogWarning("Microphone not connected!");
+        }
+        else //At least one microphone is present
+        {
+            //Set 'micConnected' to true
+            micConnected = true;
 
-						//According to the documentation, if minFreq and maxFreq are zero, the microphone supports any frequency...
-						if(minFreq == 0 && maxFreq == 0)
-						{
-								//...meaning 44100 Hz can be used as the recording sampling rate
-								maxFreq = 44100;
-						}
+            //Get the default microphone recording capabilities
+            Microphone.GetDeviceCaps(null, out minFreq, out maxFreq);
 
-						//Get the attached AudioSource component
-						goAudioSource = this.GetComponent<AudioSource>();
-				}
-	}
+            //According to the documentation, if minFreq and maxFreq are zero, the microphone supports any frequency...
+            if (minFreq == 0 && maxFreq == 0)
+            {
+                //...meaning 44100 Hz can be used as the recording sampling rate
+                maxFreq = 44100;
+            }
 
-		void OnGUI() 
-		{
-				//If there is a microphone
-				if(micConnected)
-				{
-						//If the audio from any microphone isn't being recorded
-						if(!Microphone.IsRecording(null))
-						{
-								//Case the 'Record' button gets pressed
-								if(GUI.Button(new Rect(Screen.width/2-100, Screen.height/2-25, 200, 50), "Record"))
-								{
-										//Start recording and store the audio captured from the microphone at the AudioClip in the AudioSource
-										goAudioSource.clip = Microphone.Start( null, true, 7, maxFreq); //Currently set for a 7 second clip
-								}
-						}
-						else //Recording is in progress
-						{
-								
-								//Case the 'Stop and Play' button gets pressed
-								if(GUI.Button(new Rect(Screen.width/2-100, Screen.height/2-25, 200, 50), "Stop and Play!"))
-								{
-										float filenameRand = UnityEngine.Random.Range (0.0f, 10.0f);
+            //Get the attached AudioSource component
+            goAudioSource = this.GetComponent<AudioSource>();
+        }
+    }
 
-										string filename = "testing" + filenameRand;
 
-										Microphone.End(null); //Stop the audio recording
 
-										Debug.Log( "Recording Stopped");
+    public void BeginRecord()
+    {
+        if (micConnected)
+        {
+            if (!Microphone.IsRecording(null))
+            {
+                //Start recording and store the audio captured from the microphone at the AudioClip in the AudioSource
+                goAudioSource.clip = Microphone.Start(null, true, 7, maxFreq); //Currently set for a 7 second clip
+            }
+        }
+        else
+        {
+            Debug.LogError("Microphone not connected!");
+        }
 
-										if (!filename.ToLower().EndsWith(".wav")) {
-												filename += ".wav";
-										}
+    }
 
-										var filePath = Path.Combine("testing/", filename);
-										filePath = Path.Combine(Application.persistentDataPath, filePath);
-										Debug.Log("Created filepath string: " + filePath);
+    public void StopAndSend()
+    {
+        StartCoroutine(ConvertAndSend());
+        //ConvertAndSend();
+    }
 
-										// Make sure directory exists if user is saving to sub dir.
-										Directory.CreateDirectory(Path.GetDirectoryName(filePath));
-										SavWav.Save (filePath, goAudioSource.clip); //Save a temporary Wav File
-										Debug.Log( "Saving @ " + filePath);
-										string apiURL = "http://www.google.com/speech-api/v2/recognize?output=json&lang=en-us&key=" + apiKey;
-										string Response;
+    IEnumerator ConvertAndSend()
+    {
+        float filenameRand = UnityEngine.Random.Range(0.0f, 10.0f);
+        string filename = "recordAudio" + filenameRand;
+        Microphone.End(null); //Stop the audio recording
+        Debug.Log("Recording Stopped");
 
-										Debug.Log( "Uploading " + filePath);
-										Response = HttpUploadFile (apiURL, filePath, "file", "audio/wav; rate=44100");
-										Debug.Log ("Response String: " +Response);
+        if (!filename.ToLower().EndsWith(".wav"))
+        {
+            filename += ".wav";
+        }
 
-										var jsonresponse = SimpleJSON.JSON.Parse(Response);
+        filePath = Path.Combine("recordings/", filename);
+        filePath = Path.Combine(Application.persistentDataPath, filePath);
 
-										if (jsonresponse != null) {		
-												string resultString = jsonresponse ["result"] [0].ToString ();
-												var jsonResults = SimpleJSON.JSON.Parse (resultString);
+        SavWav sav = new SavWav(filePath, goAudioSource.clip);
+        sav.Start();
+        yield return sav.WaitFor();
 
-												string transcripts = jsonResults ["alternative"] [0] ["transcript"].ToString ();
+        //Debug.Log("Saving @ " + filePath);
+        string apiURL = "YOUR API KEY"; //put your api voice key
+        //Debug.Log("Uploading " + filePath);
 
-												Debug.Log ("transcript string: " + transcripts );
-												TextBox.text = transcripts;
 
-										}
-										//goAudioSource.Play(); //Playback the recorded audio
+        StartCoroutine(HttpUploadFile(apiURL, filePath, "file", "audio/wav; rate=44100"));
+    }
 
-										File.Delete(filePath); //Delete the Temporary Wav file
-									
-								}
+    public IEnumerator HttpUploadFile(string url, string file, string paramName, string contentType)
+    {
 
-								GUI.Label(new Rect(Screen.width/2-100, Screen.height/2+25, 200, 50), "Recording in progress...");
-						}
-				}
-				else // No microphone
-				{
-						//Print a red "Microphone not connected!" message at the center of the screen
-						GUI.contentColor = Color.red;
-						GUI.Label(new Rect(Screen.width/2-100, Screen.height/2-25, 200, 50), "Microphone not connected!");
-				}
-		}
+        System.Net.ServicePointManager.ServerCertificateValidationCallback += (o, certificate, chain, errors) => true;
+        Debug.Log(string.Format("Uploading {0} to {1}", file, url));
 
-		public string HttpUploadFile(string url, string file, string paramName, string contentType) {
-				Debug.Log(string.Format("Uploading {0} to {1}", file, url));
-				HttpWebRequest wr = (HttpWebRequest)WebRequest.Create(url);
-				wr.ContentType = "audio/l16; rate=44100";
-				wr.Method = "POST";
-				wr.KeepAlive = true;
-				wr.Credentials = System.Net.CredentialCache.DefaultCredentials;
+        Byte[] bytes = File.ReadAllBytes(file);
+        String file64 = null;
+        file64 = Convert.ToBase64String(bytes, Base64FormattingOptions.None);
+        while (file64 == null)
+        {
+            yield return null;
+        }
 
-				Stream rs = wr.GetRequestStream();
-				FileStream fileStream = new FileStream(file, FileMode.Open, FileAccess.Read);
-				byte[] buffer = new byte[4096];
-				int bytesRead = 0;
-				while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) != 0) {
-						rs.Write(buffer, 0, bytesRead);
-				}
-				fileStream.Close();
-				rs.Close();
+        var json = "{ \"config\": { \"languageCode\" : \"en-US\" }, \"audio\" : { \"content\" : \"" + file64 + "\"}}";
+        var uwr = new UnityWebRequest(url, "POST");
 
-				WebResponse wresp = null;
-				try {
-						wresp = wr.GetResponse();
-						Stream stream2 = wresp.GetResponseStream();
-						StreamReader reader2 = new StreamReader(stream2);
+        byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json);
+        uwr.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonToSend);
+        uwr.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        uwr.SetRequestHeader("Content-Type", "application/json");
 
-						string responseString =  string.Format("{0}", reader2.ReadToEnd());
-						Debug.Log("HTTP RESPONSE" +responseString);
-						return responseString;
+        //Send the request then wait here until it returns
+        yield return uwr.SendWebRequest();
 
-				} catch(Exception ex) {
-						Debug.Log(string.Format("Error uploading file error: {0}", ex));
-						if(wresp != null) {
-								wresp.Close();
-								wresp = null;
-								return "Error";
-						}
-				} finally {
-						wr = null;
-			
-				}
+        if (uwr.isNetworkError)
+        {
+            Debug.Log("Error While Sending: " + uwr.error);
+        }
+        else
+        {
+            Debug.Log("Received: " + uwr.downloadHandler.text);
+            Debug.Log("Response String: " + uwr.downloadHandler.text);
 
-				return "empty";
-		}
-	
+            var jsonresponse = SimpleJSON.JSON.Parse(uwr.downloadHandler.text);
 
+            if (jsonresponse != null)
+            {
+                resultString = jsonresponse["results"][0]["alternatives"][0]["transcript"];
+                Mission.alreadyDO = false;
+                res.text = "voce disse: " + resultString;
+
+            }
+            //Playback the recorded audio
+            File.Delete(file); //Delete the Temporary Wav file
+
+
+
+        }
+    }
 }
-		
